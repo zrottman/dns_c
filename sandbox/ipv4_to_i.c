03fa21e5702165b9bcedb2fea0aca2c5f49cf327
zrottman <zrottman@example.com>
2023-07-12 08:52:34 -0700

    Add light validation and error handling

--- sandbox/ipv4_to_i.c
+++ sandbox/ipv4_to_i.c
@@ -5,29 +5,45 @@
 int main()
 {
     char c, chunk[4];
-    int i;
+    int i, chunk_as_i, chunks_count;
     uint32_t addr;
 
     i = 0;
     addr = 0;
+    chunks_count = 0;
 
     while ((c = getchar()) != EOF)
     {
 
         /* convert chunk to int if we reach a `.` or `\n` */
         if (c == '.' || c == '\n') {
-            chunk[i] = '\0';
-            //printf("Cur chunk %s -> %d\n", chunk, atoi(chunk));
-
-            addr <<= 8;
-            addr |= atoi(chunk);
+            chunk[i] = '\0';            // append null to chunk
+            addr <<= 8;                 // shift addr 8 bits left
+            chunk_as_i = atoi(chunk);   // convert current chunk to int
+            if (chunk_as_i > 255) {     // exit on invalid chunk
+                printf("Invalid IPv4 address component: %s\n", chunk);
+                return 1;
+            }
+            addr |= atoi(chunk);        // add chunk to addr
             
             // reset chunk
             for (; i>0; --i) { chunk[i] = '\0'; }
 
+            // inc chunks_count
+            chunks_count++;
+
             // output addr and reset on newline
             if (c == '\n') {
-                printf("%u\n", addr);
+                if (chunks_count == 4) {
+                    printf("%u\n", addr);
+                } else {
+                    printf("Unexpected IPv4 address format ");
+                    for (int j=1; j<chunks_count; ++j)
+                        printf("x.");
+                    printf("x\n");
+                    return 1;
+                }
+                chunks_count = 0;
                 addr = 0;
             }
 
@@ -39,9 +55,3 @@ int main()
     
     return 0;
 }
-
-/*
- * to do:
- * - handle cases where atoi(chunk) > 255
- * - handle cases where not expected number of chunks (might be better to read in full ipv4 address for this)
- */
