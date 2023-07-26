50df8fedd5f6214db3be866e2b7c0cca2b631acc
zrottman <zrottman@example.com>
2023-07-26 07:07:24 -0700

    Add documentation, add IPv6 support

--- sandbox/getaddrinfo.c
+++ sandbox/getaddrinfo.c
@@ -1,3 +1,9 @@
+/* Program:     getaddrinfo 
+ * Description: A simple program that takes human-readable domains (e.g.,
+ *              www.example.com) and uses getaddrinfo() to resolve the
+ *              IP address(es). Based on example from Beej's guide.
+ */
+
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -6,6 +12,12 @@
 
 #define MAXLINE 100
 
+// Function:    get_line
+// Description: Reads in a line character-by-character to `line`.
+// Arguments:   
+//              line:   buffer to hold characters of line
+//              lim:    max lengt of line
+// Returns:     i:      int, character count of line
 int get_line(char* line, int lim)
 {
     int  i;
@@ -22,41 +34,51 @@ int main() {
     char   line[MAXLINE];
     int    cc;
 
-    struct addrinfo hints, *p; //*res;
+    struct addrinfo hints, *p;
     int status;
     char ipstr[INET6_ADDRSTRLEN];
 
-    memset(&hints, 0, sizeof hints); // set to 0
+    // build hints struct
+    memset(&hints, 0, sizeof hints); // initialize hints struct to 0
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype  = SOCK_DGRAM;
+    // hints.ai_socktype  = SOCK_STREAM;
 
     // pack sin_addr from REPL input
     while ((cc = get_line(line, MAXLINE)) > 0) {
-        struct addrinfo *res;
+        struct addrinfo *res, *addr;
 
-        status = getaddrinfo(line, "53", &hints, &res);     // w/ hints
-        // status = getaddrinfo(line, "53", NULL, &res);    // w/o hints
+        status = getaddrinfo(line, "53", &hints, &res);     // version w/ hints
+        // status = getaddrinfo(line, "53", NULL, &res);    // version w/o hints
         printf("DNS query for %s:\n", line);
         printf("\tstatus: %d\n", status);
         
+        // traverse results linked list
         for (p = res; p != NULL; p = p->ai_next) {
 
-            if (p->ai_family == AF_INET) {
+            void *addr;
+            char *ipversion;
+
+            // parse node based on whether result is IPv4 or IPv6
+            if (p->ai_family == AF_INET) { // IPv4
                 struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
-                if (inet_ntop(AF_INET, &(ipv4->sin_addr.s_addr), ipstr, INET6_ADDRSTRLEN) != NULL)
-                    printf("\tIPv4 address: %s\n", ipstr);
-            } else {
-                // handle ipv6
-                //struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
-                //addr = &ipv6;
+                addr = &(ipv4->sin_addr.s_addr);
+                ipversion = "IPv4";
+            } else { // IPv6
+                struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+                addr = &(ipv6->sin6_addr.s6_addr);
+                ipversion = "IPv6";
             }
+
+            // print IP
+            if (inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr) != NULL)
+                printf("\t%s: %s\n", ipversion, ipstr);
 
         }
 
         freeaddrinfo(res);
 
     }
-            
 
     return 0;
 }
