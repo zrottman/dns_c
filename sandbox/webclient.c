64a6e93fbe57e2b83ec36f9f3ffa33e8556b9445
zrottman <zrottman@example.com>
2023-07-27 15:13:02 -0700

    Implement simple web client in python to troubleshoot c version (not working)

--- sandbox/webclient.c
+++ sandbox/webclient.c
@@ -3,6 +3,10 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MAXDATASIZE 4096
 
 int main(int argc, char **argv)
 {
@@ -13,39 +17,73 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    struct addrinfo *res, *p;
-    struct sockaddr_in *addr;
-    int sockfd;
-    char *hostname, *port, ipstr[INET6_ADDRSTRLEN];
+    struct addrinfo     *res;
+    struct addrinfo     *p;
+    int                 sockfd, bytes_sent, bytes_received;
+    char                *hostname;
+    char                *port;
+    char                ipstr[INET6_ADDRSTRLEN];
+    char                *request = "GET / HTTP/1.1\r\nHost: example.com\r\n\r\n";
+    char                buf[MAXDATASIZE];
+
 
     port = argv[2];
     hostname = argv[1];
 
-    // getaddrinfo()
+    // use getaddrinfo to load up addrinfo struct
     if (getaddrinfo(hostname, port, NULL, &res) != 0) {
         printf("getaddrinfo error");
         return EXIT_FAILURE;
     }
 
-    printf("RESULTS\n");
-
+    // loop through results and try to connect
     for (p = res; p != NULL; p = p->ai_next) {
-        addr = (struct sockaddr_in *)p->ai_addr;
-        inet_ntop(p->ai_family, &(addr->sin_addr), ipstr, sizeof ipstr);
-        printf("IP: %u -> %s\n", ntohl(addr->sin_addr.s_addr), ipstr);
+        // open socket
+        if ((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
+
+        // connect 
+        if (connect(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("client: connect");
+            continue;
+        }
+
+        break;
     }
 
+    if (p == NULL) {
+        perror("failure to connect");
+        return 2;
+    }
     
-
-
-    // open socket
-    //sockfd socket();
+        
     
-    // connect to socket with host and port
+    inet_ntop(p->ai_family, &((struct sockaddr_in *)p->ai_addr)->sin_addr, ipstr, sizeof ipstr);
+    printf("client: connected to: %s on port %d\n", ipstr, ntohs(((struct sockaddr_in *)p->ai_addr)->sin_port));
+    freeaddrinfo(res);
 
     // send (or sendall using Beej implementation)
     
+    bytes_sent = send(sockfd, request, strlen(request), 0);
+    printf("%d of %lu bytes sent\n", bytes_sent, strlen(request));
+    
     // recv several times to get all dta
+    printf("Response:\n");
+    while ((bytes_received = recv(sockfd, buf, MAXDATASIZE-1, 0)) > 0) {
+        buf[bytes_received] = '\0';
+        printf("%s", buf);
+    }
+
+    if (bytes_received < 0) {
+        perror("recv");
+    }
+   
+
+    // close socket
+    close(sockfd);
 
 
     return 0;
---
+++ sandbox/webclient.py
@@ -0,0 +1,38 @@
+import socket, argparse
+
+# get CLI args
+parser = argparse.ArgumentParser(
+        prog='webclient',
+        description='Receives data from host')
+
+parser.add_argument('host')
+parser.add_argument('-p', '--port', type=int)
+args = vars(parser.parse_args())
+
+host = args['host']
+port = args['port'] if args['port'] else 80
+
+# open socket
+s = socket.socket()
+
+# connect
+s.connect((host, port))
+
+# build request
+request = "GET / HTTP/1.1\r\nHOST: {}\r\n\r\n".format(host)
+request = request.encode("ISO-8859-1")
+print(request)
+
+# send request
+s.sendall(request)
+
+# receive response
+d = s.recv(4096)
+if len(d) == 0:
+    print("nothing received")
+else:
+    print(d)
+
+
+# close socket
+s.close()
