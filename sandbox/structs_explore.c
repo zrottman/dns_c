aebc1b30d5c195d96c5e4f5586d6e340bda68a69
zrottman <zrottman@example.com>
2023-07-27 09:38:27 -0700

    Relocate connect functions to new sandbox file

--- sandbox/structs_explore.c
+++ sandbox/structs_explore.c
@@ -6,25 +6,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define MAXLINE 100
-
-// Function:    get_line
-// Description: Reads in a line character-by-character to `line`.
-// Arguments:   
-//              line:   buffer to hold characters of line
-//              lim:    max lengt of line
-// Returns:     i:      int, character count of line
-int get_line(char* line, int lim)
-{
-    int  i;
-    char c;
-
-    for (i=0; i<lim-1 && (c=getchar()) != EOF && c != '\n'; i++)
-        line[i] = c;
-    line[i] = '\0';
-    return i;
-}
-
 // print size details for addrinfo
 void addrinfo_details(void)
 {
@@ -74,56 +55,12 @@ void in_addr_details(void)
 
 int main() {
 
-    char   line[MAXLINE];
-    int    cc, sockfd;
-    struct sockaddr_in addr, resolver;
-
     // print struct details
     printf("Printing struct details:\n");
     addrinfo_details();
     sockaddr_details();
     sockaddr_in_details();
     in_addr_details();
-
-
-
-    // pack resolver struct
-    resolver.sin_family = AF_INET;
-    resolver.sin_port   = htons(53);
-    inet_pton(AF_INET, "8.8.8.8", &(resolver.sin_addr));
-
-    // 1. build query
-    // 2. open socket
-    sockfd = socket(PF_INET, SOCK_DGRAM, 0);
-
-    // 3. connect?
-    if (connect(sockfd, (struct sockaddr *)&resolver, sizeof resolver) == 0) {
-        // do something
-        printf("Successful connection!\n");
-    }
-
-    // 4. send to?
-
-    // 5. deal with response
-    
-    // 6. close 
-    close(sockfd);
-
-
-
-
-    // pack default parts of sockaddr_in
-    addr.sin_family = AF_INET;
-    addr.sin_port   = htons(53);
-
-    while ((cc = get_line(line, MAXLINE)) > 0) {
-        if (inet_pton(AF_INET, line, &(addr.sin_addr)) != 1) { // invalid IP address format
-            printf("Invalid IP address format: %s\n", line);
-            exit(1);
-        }
-
-        printf("%s --> %d\n", line, ntohl(addr.sin_addr.s_addr));
-    }
 
     return 0;
 }
