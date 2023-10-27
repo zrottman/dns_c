370e94406073c7e20b1d870cc09c9966dca906ee
zrottman <zrottman@example.com>
2023-10-27 13:39:51 -0700

    Finish implementing recursive resolver (started two commits ago)

--- main.c
+++ main.c
@@ -9,18 +9,11 @@ int main(int argc, char **argv)
         printf("Expected usage: ./dns <domain name>\n");
         exit(1);
     }
-
-    DNSPacket *packet = send_query("198.41.0.4", argv[1], TYPE_A);
-    display_DNSPacket(packet);
-    destroy_DNSPacket(&packet);
-
-    /*
     char answer[INET_ADDRSTRLEN] = {0};
 
     resolve(argv[1], TYPE_A, answer);
 
     printf("%s -> %s\n", argv[1], answer);
-    */
 
     return 0;
 }
