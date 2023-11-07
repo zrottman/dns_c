be830c8fa8b21191828802f8711767e73fffaa9d
Piya <45182159+pjg11@users.noreply.github.com>
2023-11-08 02:27:38 +0400

    created sections in internals file + restructured macros

--- src/dns.h
+++ src/dns.h
@@ -9,8 +9,9 @@
 #include <unistd.h>
 #include "internals.h"
 
-#define MAX_BUFFER_SIZE 1000
+#define TYPE_A   1
+#define TYPE_NS  2
 
-void                resolve(char* domain_name, uint16_t record_type, char answer[]);
+void    resolve(char* domain_name, uint16_t record_type, char answer[]);
 
 #endif // REQUEST_H
--- src/internals.h
+++ src/internals.h
@@ -1,12 +1,16 @@
 #ifndef INTERNALS_H
 #define INTERNALS_H
 
-#define TYPE_A   1
-#define TYPE_NS  2
 #define CLASS_IN 1
+#define MAX_BUFFER_SIZE 1000
 
-// TODO: Consider defining structs in request.c but adding prototypes here, eg:
-// typedef struct DNSQuery DNSQuery;
+/* -------------------------------------------
+ * Struct Definitions
+ * ------------------------------------------- */
+
+/* The following structs are used to structure DNS queries and response packets.
+ * All integer members are stored in network byte order.
+ */
 typedef struct DNSQuery
 {
 	size_t  len;
@@ -15,7 +19,6 @@ typedef struct DNSQuery
 
 typedef struct DNSHeader
 {
-    // stored in network byte order
     uint16_t id;
     uint16_t flags;
     uint16_t num_questions;
@@ -26,7 +29,6 @@ typedef struct DNSHeader
 
 typedef struct DNSQuestion
 {
-    // stored in network byte order
     char               *name;
     uint16_t            type;       // set to TYPE_A=1 (A Record)
     uint16_t            class;      // set to CLASS_IN=1 (for internet)
@@ -35,7 +37,6 @@ typedef struct DNSQuestion
 
 typedef struct DNSRecord
 {
-    // stored in network byte order
     char             *name;       // domain name
     uint16_t          type;       // e.g., A record
     uint16_t          class;      // 1
@@ -56,39 +57,59 @@ typedef struct DNSPacket
     DNSRecord   *additionals;
 } DNSPacket;
 
+/* -------------------------------------------
+ * DNS Header
+ * ------------------------------------------- */
 static DNSHeader*   NewDNSHeader(uint16_t id, uint16_t flags, uint16_t num_questions);
-static DNSQuestion* NewDNSQuestion(char *encoded_name, uint16_t type, uint16_t class);
-static DNSQuery*    NewDNSQuery(char *domain_name, uint16_t record_type);
-static DNSPacket*   NewDNSPacket(const unsigned char *response_bytes);
-
-static int          destroy_DNSPacket(DNSPacket** packet);
-static int          destroy_DNSHeader(DNSHeader** header);
-static int	        destroy_DNSQuery(DNSQuery** query);
-static int          destroy_DNSQuestion(DNSQuestion** question);
-static int          destroy_DNSRecord(DNSRecord** record);
-
-static void         display_DNSHeader(DNSHeader *header);
-static void         display_DNSQuestion(DNSQuestion *question);
-static void         display_DNSRecord(DNSRecord *record);
-static void         display_DNSPacket(DNSPacket *packet);
-
-static size_t       encode_dns_name(char* domain_name, char* res);
-static void         header_to_bytes(DNSHeader *header, char *header_bytes);
-static void         question_to_bytes(DNSQuestion *question, char *question_bytes);
 static size_t       parse_header(const unsigned char* response_bytes, DNSHeader *header);
+static void         header_to_bytes(DNSHeader *header, char *header_bytes);
+static int          destroy_DNSHeader(DNSHeader** header);
+static void         display_DNSHeader(DNSHeader *header);
+
+/* -------------------------------------------
+ * DNS Question
+ * ------------------------------------------- */
+static DNSQuestion* NewDNSQuestion(char *encoded_name, uint16_t type, uint16_t class);
+static int          destroy_DNSQuestion(DNSQuestion** question);
+static void         display_DNSQuestion(DNSQuestion *question);
+static void         question_to_bytes(DNSQuestion *question, char *question_bytes);
 static int          parse_questions(const unsigned char *response_bytes, int bytes_read, int num_questions, DNSQuestion **head);
 static int          parse_question(const unsigned char* response_bytes, int bytes_in, DNSQuestion *question);
+
+/* -------------------------------------------
+ * DNS Query
+ * ------------------------------------------- */
+static DNSQuery*    NewDNSQuery(char *domain_name, uint16_t record_type);
+static int	        destroy_DNSQuery(DNSQuery** query);
+
+/* -------------------------------------------
+ * DNS Record
+ * ------------------------------------------- */
 static int          parse_records(const unsigned char *response_bytes, int bytes_read, int num_records, DNSRecord **head);
 static int          parse_record(const unsigned char* response_bytes, int bytes_in, DNSRecord *record);
+static int          destroy_DNSRecord(DNSRecord** record);
+static void         display_DNSRecord(DNSRecord *record);
 
-static int          decode_name(const unsigned char* response_bytes, int bytes_in, char *decoded_name);
-static int          decode_compressed_name(const unsigned char* response_bytes, int bytes_in, char *decoded_name);
+/* -------------------------------------------
+ * DNS Packet
+ * ------------------------------------------- */
+static DNSPacket*   NewDNSPacket(const unsigned char *response_bytes);
+static int          destroy_DNSPacket(DNSPacket** packet);
+static void         display_DNSPacket(DNSPacket *packet);
 
+/* -------------------------------------------
+ * DNS Resolution
+ * ------------------------------------------- */
+static DNSPacket*   send_query(char *addr, char *domain, u_int16_t type);
 static int          get_answer(DNSPacket* packet, char buf[]);
 static int          get_nameserver_ip(DNSPacket* packet, char buf[]);
 static int          get_nameserver(DNSPacket* packet, char buf[]);
 
-
-static DNSPacket *  send_query(char *addr, char *domain, u_int16_t type);
+/* -------------------------------------------
+ * Utilities
+ * ------------------------------------------- */
+static size_t       encode_dns_name(char* domain_name, char* res);
+static int          decode_name(const unsigned char* response_bytes, int bytes_in, char *decoded_name);
+static int          decode_compressed_name(const unsigned char* response_bytes, int bytes_in, char *decoded_name);
 
 #endif // INTERNALS_H
